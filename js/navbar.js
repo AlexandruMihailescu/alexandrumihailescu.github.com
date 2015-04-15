34a2dabeb75b6525e7b88503ac37374e08f6e67a
Alexandru Mihailescu <alexandru.mihailescu_dev@outlook.com>
2015-04-16 00:10:55 +0100

    92%

--- js/navbar.js
+++ js/navbar.js
@@ -79,9 +79,9 @@
     // hides the navigation’s popover
     function hideNav() {
       $('body').removeClass('noScroll');
-      $('nav').scrollTop=0;
       $('#container').removeClass('blurred');
       window.setTimeout(function(){$('body').removeClass();}, 10); // allow animations to start before removing class (Firefox)
+      $('nav').scrollTop=0;
       $('nav').removeClass('expanded');
       $('#navigation a').attr('tabindex', '-1'); // links inside hidden navigation should not be TAB selectable
       $('.icon').blur(); // deselect icon when navigation is hidden
