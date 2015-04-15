2bc6e785be90e5ddd543c03969b33ba9a19212df
Alexandru Mihailescu <alexandru.mihailescu_dev@outlook.com>
2015-04-15 19:52:37 +0100

    removed scroll prevention

--- js/preventscroll.js
+++ js/preventscroll.js
@@ -1,52 +1,4 @@
-// left: 37, up: 38, right: 39, down: 40,
-// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
-/*var keys = [32, 33, 34, 35, 36, 37, 38, 39, 40];
-
-function preventDefault(e) {
-  e = e || window.event;
-  if (e.preventDefault)
-      e.preventDefault();
-  e.returnValue = false;  
-}
-
-function keydown(e) {
-    for (var i = keys.length; i--;) {
-        if (e.keyCode === keys[i]) {
-            preventDefault(e);
-            return;
-        }
-    }
-}
-
-function wheel(e) {
-  preventDefault(e);
-}
-
-function disable_scroll() {
-
-  $(window).on("mousewheel.UserScrollDisabler DOMMouseScroll.UserScrollDisabler", this._handleWheel);
-  $(document).on("mousewheel.UserScrollDisabler touchmove.UserScrollDisabler", this._handleWheel);
-  $(document).on("keydown.UserScrollDisabler", function(event) {
-      t._handleKeydown.call(t, event);
-    });
-    
-  if (window.addEventListener) {
-      window.addEventListener('DOMMouseScroll', wheel, false);
-  }
-  window.onmousewheel = document.onmousewheel = wheel;
-  document.onkeydown = keydown;
-  $('body').bind('touchmove', function(e){e.preventDefault()});
-}
-
-function enable_scroll() {
-    if (window.removeEventListener) {
-        window.removeEventListener('DOMMouseScroll', wheel, false);
-    }
-    window.onmousewheel = document.onmousewheel = document.onkeydown = null;
-    $('body').unbind('touchmove');
-}*/
-
-var UserScrollDisabler = function() {
+/*var UserScrollDisabler = function() {
 
   // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
   // left: 37, up: 38, right: 39, down: 40
@@ -94,3 +46,4 @@ UserScrollDisabler.prototype = {
 
 
 var disabler = new UserScrollDisabler();
+*/
